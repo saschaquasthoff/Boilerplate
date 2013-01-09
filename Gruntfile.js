b536aeff2520c600474ec52dcecaec85049d3ea0
Sascha Quasthoff <sascha@quasthoff.de>
2013-01-09 11:31:39 +0100

    added standard dirs to grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,26 @@ module.exports = function(grunt) {
 
     // Config
     grunt.initConfig({
-        pkg: '<json:package.json>'
+        pkg: '<json:package.json>',
+
+        dir: {
+            // main dirs
+            source: 'source/',
+            staging: 'staging/',
+            deploy: 'deploy/',
+            vendor: 'vendor/',
+
+            assets: 'assets/',
+            sass: 'sass/',
+            css: 'styles/',
+            js: 'scripts/',
+            template: 'templates/',
+
+            // wordpress theme
+            themeDir: 'bones/',
+            wpTheme: '<%= dir.deploy %>wp-content/themes/<%= dir.themeDir %>'
+        }
+
     });
 
     // 'default' task
