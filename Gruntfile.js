1bd1c3ce73dc864f5093d25f562c62bf611d9764
Sascha Quasthoff <sascha@quasthoff.de>
2013-01-09 11:03:13 +0100

    added and updated config files

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+
+    // Config
+    grunt.initConfig({
+        pkg: '<json:package.json>'
+    });
+
+    // 'default' task
+    grunt.registerTask('default', []);
+
+};
